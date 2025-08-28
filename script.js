0d85c77ba4c3220ddbc20753746c517684ed41d7
dudaamora <eduarda.amora@escola.pr.gov.br>
2025-08-28 16:37:33 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    const texto = document.querySelector ('#entrada-de-texto').value;
+    const camporesultado = document.querySelector ('#resultado-palavrachave');
+    camporesultado.textcontent = texto;
+    const palavras = processatexto (texto);
+    camporesultado.textcontent = palavras.join ("-");
+}
+
+function processatexto(texto){
+ let palavras = texto.split("");
+ return palavras;
+ 
+}
